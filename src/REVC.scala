821b63e6785929c1c884ab821e5d41593ebc9a9d
Oskar Wantola <oskar.wantola@gmail.com>
2016-11-24 01:07:47 +0000

    Complementing a Strand of DNA problem solution

---
+++ src/REVC.scala
@@ -0,0 +1,23 @@
+import scala.io.Source._
+
+/**
+  * http://rosalind.info/problems/revc/
+  *
+  * Created by oskiw on 24/11/2016.
+  */
+object REVC extends App {
+  println(
+    fromFile("datasets/rosalind_revc.txt")
+      .mkString
+      .reverse
+      .toCharArray
+      .map{
+        case 'A' => 'T'
+        case 'T' => 'A'
+        case 'C' => 'G'
+        case 'G' => 'C'
+        case x => x
+      }
+      .mkString
+  )
+}
